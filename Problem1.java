597a7c4d1c6ee21d4780adc24901645617bec1b6
Irin Thirdwater <34905030+IrinThirdwater@users.noreply.github.com>
2018-01-02 19:21:55 +0700

    Create Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,20 @@
+/**
+ * This class prints out the solution to problem 1 of Project Euler:
+ *  "Find the sum of all the multiples of 3 or 5 below 1000."
+ *
+ * https://projecteuler.net/problem=1
+ */
+public class Problem1 {
+  
+  public static void main (String[] args) {
+    int upperBound = 1000;
+    int sumOfMultiples = 0;
+    for (int i = 1; i < upperBound; i++) {
+      if (i%3==0 || i%5==0) {
+        sumOfMultiples += i;
+      }
+    }
+    System.out.println(sumOfMultiples);
+  }
+  
+}
