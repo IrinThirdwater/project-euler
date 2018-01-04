f170e8b594b40abb5b8348ccca298b53db4adb3d
Irin Thirdwater <34905030+IrinThirdwater@users.noreply.github.com>
2018-01-04 13:44:07 +0700

    Fixed indentation issues

--- Problem1.java
+++ Problem1.java
@@ -5,17 +5,17 @@
  * https://projecteuler.net/problem=1
  */
 public class Problem1 {
-  
-  // Prints 233168, the solution to problem 1 of Project Euler.
-  public static void main (String[] args) {
-    int upperBound = 1000;
-    int sumOfMultiples = 0;
-    for (int i = 1; i < upperBound; i++) {
-      if (i%3==0 || i%5==0) {
-        sumOfMultiples += i;
-      }
+
+    // Prints 233168, the solution to problem 1 of Project Euler.
+    public static void main(String[] args) {
+        int upperBound = 1000;
+        int sumOfMultiples = 0;
+        for (int i = 1; i < upperBound; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sumOfMultiples += i;
+            }
+        }
+        System.out.println(sumOfMultiples);
     }
-    System.out.println(sumOfMultiples);
-  }
-  
+
 }
