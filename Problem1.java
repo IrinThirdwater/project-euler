9b4bd9b72281f7bb26b7cc84b06750112c4bb20e
Irin Thirdwater <34905030+IrinThirdwater@users.noreply.github.com>
2018-01-05 18:20:34 +0700

    Formatting

--- Problem1.java
+++ Problem1.java
@@ -7,7 +7,7 @@
 public class Problem1 {
 
     // Prints 233168, the solution to problem 1 of Project Euler.
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         int upperBound = 1000;
         int sumOfMultiples = 0;
         for (int i = 1; i < upperBound; i++) {
