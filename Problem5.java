6580f4dca4cbb19d36dd5586a75ec651609cf6ef
Irin Thirdwater <34905030+IrinThirdwater@users.noreply.github.com>
2018-01-05 18:19:42 +0700

    Create Problem5.java

---
+++ Problem5.java
@@ -0,0 +1,30 @@
+/**
+ * This class prints out the solution to problem 5 of Project Euler:
+ *  "Find the smallest positive number that is evenly divisible by 1, 2, ... 20."
+ *
+ * https://projecteuler.net/problem=5
+ */
+public class Problem5 {
+
+    // Prints 232792560, the solution to problem 5 of Project Euler.
+    public static void main (String[] args) {
+        int largestFactor = 20;
+        int dividee = largestFactor;
+        boolean found = false;
+
+        while (!found) {  // Let us assume such a number exists.
+            for (int i = 2; i < largestFactor; i++) {
+                if (dividee % i != 0) {
+                    dividee += largestFactor;
+                    break;
+                }
+                if (i == largestFactor - 1) {
+                    found = true;
+                }
+            }
+        }
+
+        System.out.println(dividee);
+    }
+
+}
