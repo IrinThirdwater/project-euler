e878194558a8411048c7806d30b57e42bbfddd34
Irin Thirdwater <34905030+IrinThirdwater@users.noreply.github.com>
2018-01-07 13:37:40 +0700

    Create Problem10.java

---
+++ Problem10.java
@@ -0,0 +1,24 @@
+/**
+ * This class prints out the solution to problem 10 of Project Euler:
+ *  "Find the sum of all the primes below two million."
+ *
+ * https://projecteuler.net/problem=10
+ */
+public class Problem10 {
+
+    // Prints 142913828922, the solution to problem 10 of Project Euler.
+    public static void main (String[] args) {
+        int upperBound = 2000000;
+        long sum = 5;
+
+        for (int i = 3; i < upperBound; i += 2) {
+            int sqrt = (int) Math.ceil(Math.sqrt(i));
+            for (int j = 3; j <= sqrt; j++) {
+                if (i % j == 0) break;
+                if (j == sqrt) sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
