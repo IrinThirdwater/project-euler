b80be041f899c634bb6895fac9738b1a0e3a091e
Irin Thirdwater <34905030+IrinThirdwater@users.noreply.github.com>
2018-01-11 09:41:32 +0700

    Create Problem079.java

---
+++ Problem079.java
@@ -0,0 +1,17 @@
+/**
+ * This class prints out the solution to problem 79 of Project Euler:
+ *  "Find the shortest possible passcode given a list of randomly generated triplets of 3 digits of the passcode sampled in the left to right order."
+ *
+ * https://projecteuler.net/problem=79
+ */
+public class Problem048 {
+
+    // Prints 73162890, the solution to problem 79 of Project Euler.
+    public static void main (String[] args) {
+        // Just draw a graph and put an arrow from digit A to B if A comes before B.
+        // In this case, the (directed) graph is acyclic, hence there's no repeated digits. In addition, digit 4 and 5 can be removed entirely. This leaves us with a graph containing one hamiltonian path starting from 7.
+        // Let us revisit this problem again at some point for an actual exercise in programming.
+        System.out.println(73162890);
+    }
+
+}
